4cdd1ec6d293d75b18ef3b9af404dfb08d70ce16
johnb432 <john.biselx@gmail.com>
2020-12-24 18:38:40 +0100

    Initial commit

---
+++ addons/fix_GEIST/CfgMagazineWells.hpp
@@ -0,0 +1,12 @@
+class CfgMagazineWells {
+    class CBA_762x54R_Mosin {
+        WW2FixGEIST[] = {
+            "GLIB_FIN_5Rnd_762x54mm"
+        };
+    };
+    class CBA_75x54mmFrench_MAS36 {
+        WW2FixGEIST[] = {
+            "GLIB_FRA_5Rnd_75x54mm"
+        };
+    };
+};
---
+++ addons/fix_GEIST/config.cpp
@@ -0,0 +1,20 @@
+class CfgPatches {
+    class fixGEIST {
+        name = "Fix GEIST";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.96;
+        requiredAddons[] = {
+            "geistl_assets_c_weapons",
+            "geistl_assets_t_weapons"
+        };
+        author = "FK Gaming";
+        authors[] = {
+            "johnb43"
+        };
+    };
+};
+
+//#include "CfgWeapons.hpp"
+//#include "CfgMagazines.hpp"
+#include "CfgMagazineWells.hpp"
